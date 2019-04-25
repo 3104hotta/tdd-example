0e1d566d74259efe5896b708e026aa66ac6523b7
3104hottest <Hotta3104>
2019-04-25 13:14:38 +0900

    p.5

--- money/MoneyTest.java
+++ money/MoneyTest.java
@@ -1,13 +1,14 @@
 
 package money;
 
+import Dollar.Dollar;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MoneyTest {
     @Test
     public void testMultiplication() {
-        Dollar five = new Dollar(5);
+        money.Dollar five = new Dollar(5);
         five.times(2);
         assertEquals(10, five.amount);
     }
