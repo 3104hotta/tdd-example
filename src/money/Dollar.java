2625c6124c2ffc34e1c72f1878bb4aa76a3076b0
Satoshi Hotta <3104hotta@gmail.com>
2019-04-28 15:29:34 +0900

    p.61

--- src/money/Dollar.java
+++ src/money/Dollar.java
@@ -1,12 +1,12 @@
 
 package money;
 
-class Dollar extends Money{
-    Dollar(int amount) {
+class Dollar extends Money {
+    Dollar(int amount, String currency) {
         this.amount = amount;
-        currency = "USD";
+        this.currency = currency;
     }
     Money times(int multiplier) {
-        return new Dollar(amount * multiplier);
+        return Money.dollar(amount * multiplier);
     }
 }
--- src/money/MoneyTest.java
+++ src/money/MoneyTest.java
@@ -9,8 +9,8 @@ public class MoneyTest {
     @Test
     public void testMultiplication() {
         Money five = Money.dollar(5);
-        assertEquals(new Dollar(10), five.times(2));
-        assertEquals(new Dollar(15), five.times(3));
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
     }
     @Test
     public void testEquality (){
