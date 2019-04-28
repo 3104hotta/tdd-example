baf900a7fb1b63f889db16f11506fe8b4ddcabe2
Satoshi Hotta <3104hotta@gmail.com>
2019-04-28 15:59:24 +0900

    p.71

--- src/money/Money.java
+++ src/money/Money.java
@@ -3,12 +3,12 @@ package money;
 class Money {
     protected int amount;
     protected String currency;
-    Money times (int multiplier) {
-        return null;
-    }
     Money(int amount , String currency) {
         this.amount = amount;
         this.currency = currency;
+    }
+    Money times (int multiplier) {
+        return new Money(amount * multiplier, currency);
     }
     String currency() {
         return currency;
