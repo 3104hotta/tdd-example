ad03e35560a673e609e5285d0a405c9c7d7376e9
Satoshi Hotta <3104hotta@gmail.com>
2019-04-29 13:10:53 +0900

    p.98

--- src/money/Money.java
+++ src/money/Money.java
@@ -21,6 +21,9 @@ class Money implements Expression {
     Expression plus (Money addend) {
         return new Sum (this, addend);
     }
+    public Money reduce(String to) {
+        return this;
+    }
     public String toString() {
         return amount + " " + currency;
     }
