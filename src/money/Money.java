b6f9ce6d6f01742f604ece067cc37da0a02a25f6
Satoshi Hotta <3104hotta@gmail.com>
2019-04-30 10:51:09 +0900

    p.102

--- src/money/Money.java
+++ src/money/Money.java
@@ -22,7 +22,8 @@ class Money implements Expression {
         return new Sum (this, addend);
     }
     public Money reduce(String to) {
-        return this;
+        int rate = (currency.equals("CHF") && to.equals("USD")) ? 2: 1;
+        return new Money ( amount / rate, to);
     }
     public String toString() {
         return amount + " " + currency;
