48885b22fc1ff93966a8b79c26a1350a265833b4
Satoshi Hotta <satoshi.hotta@axa.co.jp>
2019-05-10 09:10:06 +0900

    p.150

--- src/xunit.py
+++ src/xunit.py
@@ -5,6 +5,7 @@ class TestCase:
     def run(self):
         method = getattr(self, self.name)
         method()
+
 class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
@@ -12,10 +13,11 @@ class WasRun(TestCase):
     def testMethod(self):
         self.wasRun = 1
 
+class TestCaseTest(TestCase):
+    def testRunning(self):
+        test = WasRun("testMethod")
+        assert(not test.wasRun)
+        test.run()
+        assert(test.wasRun)
 
-
-test = WasRun("testMethod")
-print(test.wasRun)
-test.run()
-print(test.wasRun)
-
+TestCaseTest("testRunning").run()
