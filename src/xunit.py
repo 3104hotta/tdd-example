712971830b66de3a70919286c76884ce4538ae5e
Satoshi Hotta <3104hotta@gmail.com>
2019-05-12 14:35:09 +0900

    p.164

--- src/xunit.py
+++ src/xunit.py
@@ -1,4 +1,8 @@
 
+class TestResult:
+    def summary(self):
+        return "1 run, 0 failed"
+
 class TestCase:
     def __init__(self, name):
         self.name = name
@@ -11,6 +15,7 @@ class TestCase:
         method = getattr(self, self.name)
         method()
         self.tearDown()
+        return TestResult()
 
 class WasRun(TestCase):
     def setUp(self):
@@ -25,5 +30,10 @@ class TestCaseTest(TestCase):
         test = WasRun("testMethod")
         test.run()
         assert("setUp testMethod tearDown " == test.log)
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert("1 run, 0 failed" == result.summary())
 
 TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
